7fb4b72f0599b1eee6804e25cc89f9b66bf1e778
Tim Lucas <t@toolmantim.com>
2015-10-28 19:10:24 +1100

    Fix the key

--- hooks/commands/build.sh
+++ hooks/commands/build.sh
@@ -8,21 +8,25 @@ service_docker_image_name() {
   docker_compose_container_name "$(compose_service_name)"
 }
 
+build_meta_data_artifact_key() {
+  echo "docker-compose-plugin-built-image-artifact-$(service_docker_image_name)"
+}
+
 save_image_as_artifact() {
   # Name the tgz the same as the service docker image name
-  local image_tgz_path="$(service_docker_image_name).tgz"
+  local image_tgz_path="$(compose_service_name)-build-$BUILDKITE_BUILD_NUMBER.tgz"
 
   # Save down the image
   buildkite-run "docker save $(service_docker_image_name) | gzip -c > $image_tgz_path"
 
   # Upload the image
-  buildkite-agent artifact upload "$image_tgz_path"
+  buildkite-run "buildkite-agent artifact upload \"$image_tgz_path\""
 
   # Store as build meta-data
-  buildkite-agent meta-data set "docker-compose-plugin-built-image-artifact-$(service_docker_image_name)", "$image_tgz_path"
+  buildkite-run "buildkite-agent meta-data set \"$(build_meta_data_artifact_key)\" \"$image_tgz_path\""
 }
 
-echo "~~~ Building Docker Compose images for service $(service_docker_image_name)"
+echo "+++ Building Docker Compose images for service $(service_docker_image_name)"
 
 run_docker_compose "build" "$(service_docker_image_name)"
 
