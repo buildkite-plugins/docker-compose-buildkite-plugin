dc1fa3b09ca13e7c54400fa2fee3d0f5736a9cd6
Luke Fries <lfries@cozero.com.au>
2016-09-20 15:54:21 +1000

    update

--- hooks/commands/run.sh
+++ hooks/commands/run.sh
@@ -62,7 +62,7 @@ try_image_restore_from_docker_repository() {
     # TODO: Fix this el-dodgo method
     local escaped_tag_for_sed=$(echo "$tag" | sed -e 's/[\/&]/\\&/g')
     # dogde ++
-    if [[ ! -z "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_EXCONFIG:-}" ]]; then
+    if [[ ! -z "$BUILDKITE_PLUGIN_DOCKER_COMPOSE_EXCONFIG" ]]; then
       plugin_prompt_and_must_run sed -i.orig "s/build: \./image: $escaped_tag_for_sed/" "docker-compose.yml"
     else
       plugin_prompt_and_must_run sed -i.orig "s/build: \./image: $escaped_tag_for_sed/" "$(docker_compose_config_file)"
