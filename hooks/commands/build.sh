d22b8d7ad94ff0befef914567001d37a50317017
Samuel Cochran <sj26@sj26.com>
2016-10-04 13:41:45 +1100

    Support adding arbitrary docker image tags

--- hooks/commands/build.sh
+++ hooks/commands/build.sh
@@ -15,15 +15,26 @@ image_file_name() {
 }
 
 push_image_to_docker_repository() {
-  local tag="$DOCKER_IMAGE_REPOSITORY:$(image_file_name)"
-  local sha_tag="$DOCKER_IMAGE_REPOSITORY:$(short_sha)"
+  # XXX: Consuming array configuration is pretty gnarly
+  declare -a tags
+  if [[ -n "$BUILDKITE_PLUGIN_DOCKER_COMPOSE_TAGS_0" ]]; then
+    local i=0
+    local name="BUILDKITE_PLUGIN_DOCKER_COMPOSE_TAGS_${i}"
+    while [[ -n "${!name:-}" ]]; do
+      tags+=("${!name}")
+      i=$[$i+1]
+      name="BUILDKITE_PLUGIN_DOCKER_COMPOSE_TAGS_${i}"
+    done
+  else
+    tags+=("$(image_file_name)")
+  fi
 
-  plugin_prompt_and_must_run docker tag "$COMPOSE_SERVICE_DOCKER_IMAGE_NAME" "$tag"
-  plugin_prompt_and_must_run docker tag "$COMPOSE_SERVICE_DOCKER_IMAGE_NAME" "$sha_tag"
-  plugin_prompt_and_must_run docker push "$tag"
-  plugin_prompt_and_must_run docker push "$sha_tag"
-  plugin_prompt_and_must_run docker rmi "$tag"
-  plugin_prompt_and_must_run docker rmi "$sha_tag"
+  local tag
+  for tag in "${tags[@]}"; do
+    plugin_prompt_and_must_run docker tag "$COMPOSE_SERVICE_DOCKER_IMAGE_NAME" "$DOCKER_IMAGE_REPOSITORY:$tag"
+    plugin_prompt_and_must_run docker push "$DOCKER_IMAGE_REPOSITORY:$tag"
+    plugin_prompt_and_must_run docker rmi "$DOCKER_IMAGE_REPOSITORY:$tag"
+  done
 
   plugin_prompt_and_must_run buildkite-agent meta-data set "$(build_meta_data_image_tag_key "$COMPOSE_SERVICE_NAME")" "$tag"
 }
