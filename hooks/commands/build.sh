fc83cd420589815437b560e5090ea89823de5544
Tim Lucas <t@toolmantim.com>
2017-01-09 16:47:35 +1100

    Remove the local container name from "Pushing image..." group
    Removes the need for docker_compose_container_name here

--- hooks/commands/build.sh
+++ hooks/commands/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 COMPOSE_SERVICE_NAME="$BUILDKITE_PLUGIN_DOCKER_COMPOSE_BUILD"
-COMPOSE_SERVICE_DOCKER_IMAGE_NAME="$(docker_compose_container_name "$COMPOSE_SERVICE_NAME")"
 DOCKER_IMAGE_REPOSITORY="${BUILDKITE_PLUGIN_DOCKER_COMPOSE_IMAGE_REPOSITORY:-}"
 COMPOSE_SERVICE_OVERRIDE_FILE="docker-compose.buildkite-$COMPOSE_SERVICE_NAME-override.yml"
 IMAGE_NAME="${BUILDKITE_PROJECT_SLUG}-${COMPOSE_SERVICE_NAME}-build-${BUILDKITE_BUILD_NUMBER}"
@@ -35,7 +34,7 @@ echo "+++ :docker: Building Docker Compose images for service $COMPOSE_SERVICE_N
 run_docker_compose -f "$COMPOSE_SERVICE_OVERRIDE_FILE" build "$COMPOSE_SERVICE_NAME"
 
 if [[ ! -z "$DOCKER_IMAGE_REPOSITORY" ]]; then
-  echo "~~~ :docker: Pushing image $COMPOSE_SERVICE_DOCKER_IMAGE_NAME to $DOCKER_IMAGE_REPOSITORY"
+  echo "~~~ :docker: Pushing image to $DOCKER_IMAGE_REPOSITORY"
 
   push_image_to_docker_repository
 fi
