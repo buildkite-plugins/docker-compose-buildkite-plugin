8101d4665f5c4391957ad86e9a3f6d02f928d212
Lachlan Donald <lachlan@ljd.cc>
2017-04-23 15:13:41 +1000

    Fix undefined TAG variable error

--- hooks/commands/build.sh
+++ hooks/commands/build.sh
@@ -19,6 +19,6 @@ run_docker_compose -f "$override_file" build "$build_service_name"
 
 if [[ ! -z "$build_image_repository" ]]; then
   echo "~~~ :docker: Pushing image to $build_image_repository"
-  plugin_prompt_and_must_run docker push "$TAG"
-  plugin_prompt_and_must_run buildkite-agent meta-data set "$(build_meta_data_image_tag_key "$build_service_name")" "$TAG"
+  plugin_prompt_and_must_run docker push "$build_image_name"
+  plugin_prompt_and_must_run buildkite-agent meta-data set "$(build_meta_data_image_tag_key "$build_service_name")" "$build_image_name"
 fi
