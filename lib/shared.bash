1d00331cc0a08ce83b34d4f795691914123e30c8
Lachlan Donald <lachlan@ljd.cc>
2017-04-23 15:14:21 +1000

    Move HIDE_PROMPT env check higher up

--- lib/shared.bash
+++ lib/shared.bash
@@ -1,14 +1,11 @@
 
 # Show a prompt for a command
 function plugin_prompt() {
-  # Output "$" prefix in a pleasant grey...
-  echo -ne "\033[90m$\033[0m"
-
-  # ...each positional parameter with spaces and correct escaping for copy/pasting...
-  printf " %q" "$@"
-
-  # ...and a trailing newline.
-  echo
+  if [[ -z "${HIDE_PROMPT:-}" ]] ; then
+    echo -ne "\033[90m$\033[0m"
+    printf " %q" "$@"
+    echo
+  fi
 }
 
 # Shows the command being run, and runs it
@@ -91,11 +88,7 @@ function run_docker_compose() {
 
   command+=(-p "$(docker_compose_project_name)")
 
-  if [[ -z "${HIDE_PROMPT:-}" ]] ; then
-    plugin_prompt_and_run "${command[@]}" "$@"
-  else
-    "${command[@]}" "$@"
-  fi
+  plugin_prompt_and_run "${command[@]}" "$@"
 }
 
 function build_meta_data_image_tag_key() {
