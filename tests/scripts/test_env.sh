5d61e9b52fb7d6efd9f29f804b0c2d53ece08861
Lachlan Donald <lachlan@buildkite.com>
2017-09-06 11:38:03 +1000

    Fix relative path for volume mount

--- tests/scripts/test_env.sh
+++ tests/scripts/test_env.sh
@@ -4,9 +4,13 @@ set -eu
 if ! [ "$LLAMAS" = always ] ; then
   echo "Expected \$LLAMAS=always, got $LLAMAS"
   exit 1
+else
+  echo "LLAMAS=always 👌🏻"
 fi
 
 if ! [ "$ALPACAS" = sometimes ] ; then
   echo "Expected \$ALPACAS=sometimes, got $ALPACAS"
   exit 1
+else
+  echo "ALPACAS=sometimes 👌🏻"
 fi
