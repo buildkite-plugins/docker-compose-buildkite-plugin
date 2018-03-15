4bb922f0679948541bc1f60912a4a9c3d29e2913
Tim Lucas <t@toolmantim.com>
2018-03-15 21:09:49 +1100

    Extract the image-name setter into metadata lib

--- lib/metadata.bash
+++ lib/metadata.bash
@@ -19,8 +19,20 @@ function plugin_set_metadata() {
   plugin_prompt_and_must_run buildkite-agent meta-data set "$key" "$value"
 }
 
-# Gets a prebuilt iamge for a service name
+function prebuilt_image_meta_data_key() {
+  local service="$1"
+  echo "${META_IMAGE_TAG}${service}"
+}
+
+# Sets a prebuilt image for a service name
+function set_prebuilt_image() {
+  local service="$1"
+  local image="$2"
+  plugin_set_metadata "$(prebuilt_image_meta_data_key "$service")" "$image"
+}
+
+# Gets a prebuilt image for a service name
 function get_prebuilt_image() {
   local service="$1"
-  plugin_get_metadata "${META_IMAGE_TAG}${service}"
+  plugin_get_metadata "$(prebuilt_image_meta_data_key "$service")"
 }
