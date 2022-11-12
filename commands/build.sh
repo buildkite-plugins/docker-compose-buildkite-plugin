2f9af14c10deedbce69581343875c9ba370b3db4
Matías A. Bellone <matias.bellone@buildkite.com>
2022-11-12 00:49:40 -0300

    Add --secrets parameters on build

--- commands/build.sh
+++ commands/build.sh
@@ -157,6 +157,11 @@ if [[ "$(plugin_read_config BUILD_PARALLEL "false")" == "true" ]] ; then
   build_params+=(--parallel)
 fi
 
+# Parse the list of secrets to pass on to build command
+while read -r line ; do
+  [[ -n "$line" ]] && build_params+=("--secret" "$line")
+done <<< "$(plugin_read_list SECRETS)"
+
 if [[ "$(plugin_read_config SSH "false")" == "true" ]] ; then
   if [[ "${DOCKER_BUILDKIT:-}" != "1" && "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CLI_VERSION:-}" != "2" ]]; then
     echo "🚨 You can not use the ssh option if you are not using buildkit"
