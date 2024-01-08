49b16098526563af6cad76876e5811d6e05772f1
Matías A. Bellone <matias.bellone@buildkite.com>
2024-01-08 15:33:47 -0300

    Correct CLI default wiht SSH option

--- commands/build.sh
+++ commands/build.sh
@@ -95,7 +95,7 @@ while read -r line ; do
 done <<< "$(plugin_read_list SECRETS)"
 
 if [[ "$(plugin_read_config SSH "false")" != "false" ]] ; then
-  if [[ "${DOCKER_BUILDKIT:-}" != "1" && "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CLI_VERSION:-}" != "2" ]]; then
+  if [[ "${DOCKER_BUILDKIT:-}" != "1" && "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CLI_VERSION:-2}" != "2" ]]; then
     echo "🚨 You can not use the ssh option if you are not using buildkit"
     exit 1
   fi
