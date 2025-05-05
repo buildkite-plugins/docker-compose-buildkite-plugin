4f4b65a20626c6e769dc7d1cd58ac7965166aadf
Matías A. Bellone <matias.bellone@buildkite.com>
2025-05-05 19:25:07 -0300

    Fix windows bug with default compose separator

--- lib/shared.bash
+++ lib/shared.bash
@@ -114,9 +114,19 @@ function docker_compose_config_files() {
     return
   fi
 
+  # If COMPOSE_PATH_SEPARATOR is not set, use the default separator
+  SEPARATOR="${COMPOSE_PATH_SEPARATOR:-}"
+  if [ -z ${SEPARATOR} ]; then
+    if is_windows ; then
+      SEPARATOR=";"
+    else
+      SEPARATOR=":"
+    fi
+  fi
+
   # Process any (deprecated) colon delimited config paths
   for value in "${config_files[@]}" ; do
-    echo "$value" | tr ':' '\n'
+    echo "$value" | tr "${SEPARATOR}" '\n'
   done
 }
 
